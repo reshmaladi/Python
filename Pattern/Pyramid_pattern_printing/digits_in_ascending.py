2009286bdb245538cd7a0a1643e63bc413f5d0a5
reshma.ladi <reshma.ladi@gmail.com>
2021-04-20 08:07:44 +0530

    Update digits_in_ascending.py

--- Pattern/Pyramid_pattern_printing/digits_in_ascending.py
+++ Pattern/Pyramid_pattern_printing/digits_in_ascending.py
@@ -1,6 +1,8 @@
-n =int(input("Enter the numer of rows:"))
-for i in range(n):
-	print (" " * (n-i-1), end ="")
-	for j in range (i+1):
-		print (j+1, end = "")
-	print()
+#!C:\Python34
+if __name__ == "__main__":
+	n =int(input("Enter the numer of rows:"))
+	for i in range(n):
+		print (" " * (n-i-1), end ="")
+		for j in range(i+1):
+			print (j+1, end = " ")
+		print()
